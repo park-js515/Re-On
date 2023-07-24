9c8842b88997c65f1285c3915c3e1e10ccc63598
marugy <gyulife7301@naver.com>
2023-07-24 17:17:26 +0900

    S09P12C203-128 / refact / RE:ON 오타 수정

--- backend/src/main/java/reon/app/ReonApplication.java
+++ backend/src/main/java/reon/app/ReonApplication.java
@@ -2,6 +2,7 @@ package reon.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class ReonApplication {
