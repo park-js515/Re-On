dc9c4c26e891f4b0e434ba594673c38784d3272a
marugy <gyulife7301@naver.com>
2023-07-25 15:39:45 +0900

    S09P12C203-129 / feat / memberApi 작성

--- backend/src/main/java/reon/app/domain/member/api/MemberApi.java
+++ backend/src/main/java/reon/app/domain/member/api/MemberApi.java
@@ -3,13 +3,22 @@ package reon.app.domain.member.api;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@Api(tags = {"member"})
+@Api(tags = {"User"})
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("api/user-management")
 public class MemberApi {
 
+    private final MemberService memberService;
+
+    @ApiOperation(value="유저 정보 조회", notes = "해당 유저의 정보 조회")
+    @PostMapping("/user/{id}")
+    public ResponseEntity<?> getMember(@PathVariable Long id){
+        MemberResponse member = memberService.findById(id);
+        return ResponseEntity<>(member);
+    }
 }
