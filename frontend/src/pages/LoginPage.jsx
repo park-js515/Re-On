af65b051a827f270f44729a298e6b0d18acb4d7e
이명인 <dldlfqo8@naver.com>
2023-07-25 11:43:18 +0900

    S09P12C203-258 / feat /  이미지 폴더와 로그인 Page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,17 @@
+import ImageCarousel from "../components/Login/imageCarousel.jsx"
+import "../components/Login/ImageCarousel.css"
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className=" w-full h-screen grid-carousel bg-gradient-to-tl from-begie to-lightBlue flex justify-center items-center  ">
+      <div className=" flex  w-4/5 bg-white h-4/5  max-w-[1000px] min-w-[700px] rounded-2xl p-4 pb-0 shadow-xl ">
+        <div className="w-1/2 h-full  rounded-2xl mt-5 ">
+          <ImageCarousel />
+        </div>
+      </div>
+    </div>
+  )
+
 };
 
 export default LoginPage;
