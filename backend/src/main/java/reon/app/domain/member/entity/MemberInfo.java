52294acce36ad8c66747988ad301ed2aaa6ce0ed
marugy <gyulife7301@naver.com>
2023-07-25 17:02:42 +0900

    S09P12C203-129 / feat / MemberInfo Entity 생성

--- backend/src/main/java/reon/app/domain/member/entity/MemberInfo.java
+++ backend/src/main/java/reon/app/domain/member/entity/MemberInfo.java
@@ -5,13 +5,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 import reon.app.global.entity.BaseEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Entity
@@ -23,12 +21,59 @@ public class MemberInfo extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(name = "nickName", nullable = false)
     private String nickName;
+    
+    @Column(name = "profileImg")
     private String profileImg;
+    
+    @Column(name = "tier")
+    // TODO: 2023-07-25 티어 결정 시 기본 티어 default 생성
+//    @ColumnDefault("브론즈")
     private String tier;
-    private String content;
+    
+    @Column(name = "introduce")
+    private String introduce;
+    
+    @Column(name = "gameCnt")
+    @ColumnDefault("0")
     private int gameCnt;
+    
+    @Column(name = "win")
+    @ColumnDefault("0")
     private int win;
+
+    @Column(name = "lose")
+    @ColumnDefault("0")
     private int lose;
+
+    @Column(name = "reported")
+    @ColumnDefault("0")
     private int reported;
+
+    public void updateNickName(String nickName){
+        this.nickName = nickName;
+    }
+    public void updateProfileImg(String profileImg){
+        this.profileImg = profileImg;
+    }
+    public void updateTier(String tier){
+        this.tier = tier;
+    }
+    public void updateContent(String introduce){
+        this.introduce = introduce;
+    }
+    public void updateGameCnt(){
+        this.gameCnt += 1;
+    }
+    public void updateWin(){
+        this.win += 1;
+    }
+    public void updateLose(){
+        this.lose += 1;
+    }
+    public void updateReported(int reportCnt){
+        this.reported = reportCnt;
+    }
 }
