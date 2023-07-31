4e1a1449626a02f1d4147ee5d34cb24fe60a39b2
안종상 <skqlck-@naver.com>
2023-07-31 17:55:13 +0900

    S09P12C203-253 / feat / 영상 썸네일

---
+++ frontend/src/components/VideoComponent/Videoitem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {Card,
+        CardContent,
+        Typography,
+        CardMedia,
+    } from '@mui/material/Card';
+
+const Videoitem = null;
---
+++ frontend/src/components/VideoComponent/Videolist.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Videolist = () => {
+
+    return(
+        <div>
+
+        </div>
+)}
+
+export default Videolist
