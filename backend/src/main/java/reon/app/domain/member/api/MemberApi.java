209161f18136aecb4f7d50c81ae0a170bfe44a0a
marugy <gyulife7301@naver.com>
2023-07-31 22:00:10 +0900

    S09P12C203-326 / feat / ApiError 객체 및 ApiResponse 객체 생성

--- backend/src/main/java/reon/app/domain/member/api/MemberApi.java
+++ backend/src/main/java/reon/app/domain/member/api/MemberApi.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.*;
 @Api(tags = {"member"})
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api/member-management")
 public class MemberApi {
+
+
+
 
 }
