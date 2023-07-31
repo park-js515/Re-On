ee97b575e4e38464059e95c97aca7c46ba15db02
marugy <gyulife7301@naver.com>
2023-08-01 00:37:22 +0900

    S09P12C203-326 / feat / MemberBattleInfo Entity 분리 및 UpdateRequest 구현

---
+++ backend/src/main/java/reon/app/domain/member/dto/request/MemberBattleInfoUpdateRequest.java
@@ -0,0 +1,27 @@
+package reon.app.domain.member.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import reon.app.domain.member.entity.MemberBattleInfo;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class MemberBattleInfoUpdateRequest {
+    private int score;//누적 점수
+    private int gameCnt;//게임 수
+    private int win;//승리
+    private int lose;//패배
+
+    public MemberBattleInfo toEntity(){
+        return MemberBattleInfo.builder()
+                .score(this.score)
+                .gameCnt(this.gameCnt)
+                .win(this.win)
+                .lose(this.lose)
+                .build();
+    }
+}
