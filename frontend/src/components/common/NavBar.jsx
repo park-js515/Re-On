402a1e3c1a60c01dba71b6fb51fd3dce83197d23
이명인 <dldlfqo8@naver.com>
2023-08-01 17:12:36 +0900

    S09P12C203-283 / feat / 네브바 수정 및 메인페이지

--- frontend/src/components/common/NavBar.jsx
+++ frontend/src/components/common/NavBar.jsx
@@ -30,14 +30,14 @@
 
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-
+import { Link } from 'react-router-dom'
 
 const navigation = [
-  { name: '메인페이지', href: '/', current: true },
-  { name: '랭크게임', href: '/backstage', current: false },
-  { name: '일반게임', href: '/normallist', current: false },
-  { name: '투표해줘', href: '/feed', current: false },
-  { name: '로그인', href: '/login', current: false },
+  { name: '메인페이지', to: '/', current: false },
+  { name: '랭크게임', to: '/backstage', current: false },
+  { name: '일반게임', to: '/normallist', current: false },
+  { name: '투표해줘', to: '/feed', current: false },
+  { name: '로그인', to: '/login', current: false },
 ]
 
 function classNames(...classes) {
@@ -46,29 +46,26 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
+    <div className="sticky top-0 z-50">
     <Disclosure as="nav" className="bg-dark-800">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-           
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                
                   <img
                     className="h-10 w-auto"
                     src="/image/logo/logo.png"
                     alt="Your Company"
-
                   />
-         
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <Link
                         key={item.name}
-                        href={item.href}
+                        to={item.to}
                         className={classNames(
                           item.current ? 'bg-gray-900 text-lightBlue' : 'text-gray-300 hover:bg-gray-700 hover:text-lightBlue',
                           'rounded-md px-3 py-2 text-sm font-medium'
@@ -76,15 +73,12 @@ export default function Example() {
                         aria-current={item.current ? 'page' : undefined}
                       >
                         {item.name}
-                      </a>
+                      </Link>
                     ))}
                   </div>
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                
-
-                {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-lightBlue focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -97,29 +91,26 @@ export default function Example() {
                       />
                     </Menu.Button>
                   </div>
-                  <Transition
-                    as={Fragment}
-                 
-                  >
+                  <Transition as={Fragment}>
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="/"
+                          <Link
+                            to="/profile"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             마이페이지
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="/"
+                          <Link
+                            to="/logout"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             로그아웃
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                     </Menu.Items>
@@ -128,14 +119,13 @@ export default function Example() {
               </div>
             </div>
           </div>
-
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
                 <Disclosure.Button
+                  as={Link}
                   key={item.name}
-                  as="a"
-                  href={item.href}
+                  to={item.to}
                   className={classNames(
                     item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                     'block rounded-md px-3 py-2 text-base font-medium'
@@ -150,5 +140,6 @@ export default function Example() {
         </>
       )}
     </Disclosure>
+    </div>
   )
 }
