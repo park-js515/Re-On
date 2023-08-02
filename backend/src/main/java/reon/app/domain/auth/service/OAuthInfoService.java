5db910a9c155ea59b09db1a1a57aa39ccbe5cc83
jinsikhong <eat159357@kookmin.ac.kr>
2023-08-03 00:56:13 +0900

    S09P12C203-329 / feat / AuthToken, JwtToken, service 구현

---
+++ backend/src/main/java/reon/app/domain/auth/service/OAuthInfoService.java
@@ -0,0 +1,37 @@
+package reon.app.domain.auth.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reon.app.domain.auth.apiClient.OAuthApiClient;
+import reon.app.domain.auth.dto.res.OAuthInfoResponse;
+import reon.app.domain.auth.params.OAuthLoginParams;
+import reon.app.domain.member.entity.OAuthProvider;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
+@Slf4j
+public class OAuthInfoService {
+    private final Map<OAuthProvider, OAuthApiClient> clients;
+
+    public OAuthInfoService(List<OAuthApiClient> clients) {
+        this.clients = clients.stream().collect(
+                Collectors.collectingAndThen(
+                        Collectors.toMap(OAuthApiClient::oAuthProvider, Function.identity()),
+                        Collections::unmodifiableMap
+                )
+        );
+    }
+
+    public OAuthInfoResponse request(OAuthLoginParams params) {
+        OAuthApiClient client = clients.get(params.oAuthProvider());
+        log.info(client.toString());
+        String accessToken = client.requestAccessToken(params);
+
+        return client.requestOAuthInfo(accessToken);
+    }
+}
