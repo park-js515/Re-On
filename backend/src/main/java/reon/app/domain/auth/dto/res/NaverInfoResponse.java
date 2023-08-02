1f92b1c4045f1203d0f52c38c455ee1e87d30ed2
jinsikhong <eat159357@kookmin.ac.kr>
2023-08-02 23:54:10 +0900

    S09P12C203-329 / feat / InfoResponse 작성

---
+++ backend/src/main/java/reon/app/domain/auth/dto/res/NaverInfoResponse.java
@@ -0,0 +1,54 @@
+package reon.app.domain.auth.dto.res;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import reon.app.domain.member.entity.OAuthProvider;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class NaverInfoResponse implements OAuthInfoResponse {
+
+    @JsonProperty("response")
+    private Response response;
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class Response {
+        private String email;
+        private String nickname;
+        private String gender;
+        private String birthday;
+        private String profile_image;
+    }
+
+    @Override
+    public String getEmail() {
+        return response.getEmail();
+    }
+
+    @Override
+    public String getNickName() {
+        return response.getNickname();
+    }
+
+    @Override
+    public String getGender() {
+        return response.getGender();
+    }
+
+    @Override
+    public String getBirthday() {
+        return response.getBirthday();
+    }
+
+    @Override
+    public String getProfileImage() {
+        return response.getProfile_image();
+    }
+
+    @Override
+    public OAuthProvider getOAuthProvider() {
+        return OAuthProvider.NAVER;
+    }
+}
---
+++ backend/src/main/java/reon/app/domain/auth/dto/res/OAuthInfoResponse.java
@@ -0,0 +1,14 @@
+package reon.app.domain.auth.dto.res;
+
+import reon.app.domain.member.entity.OAuthProvider;
+
+public interface OAuthInfoResponse {
+    // 이름, 이메일, 생년월일, 성별, 닉네임, 프로필 사진
+    // 로그인 도메인 추가 시 수정 필요
+    String getEmail();
+    String getNickName();
+    String getGender();
+    String getBirthday();
+    String getProfileImage();
+    OAuthProvider getOAuthProvider();
+}
