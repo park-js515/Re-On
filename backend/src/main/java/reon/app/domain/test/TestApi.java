0d3554616d21c4192b500dab58b9d29ac865df34
marugy <gyulife7301@naver.com>
2023-08-03 01:24:26 +0900

    S09P12C203-135 / feat / openvidu controller

--- backend/src/main/java/reon/app/domain/test/TestApi.java
+++ backend/src/main/java/reon/app/domain/test/TestApi.java
@@ -2,6 +2,7 @@ package reon.app.domain.test;
 
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/test")
 public class TestApi {
-
+    @Value("${OPENVIDU_SECRET}")
+    private String OPENVIDU_SECRET;
     @GetMapping("/")
     public String test(){
-        return "hello";
+        return this.OPENVIDU_SECRET;
     }
 }
