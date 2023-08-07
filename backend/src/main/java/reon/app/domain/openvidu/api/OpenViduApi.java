fe2f9b1fcd3f5ebbef32a2a82f7f5082d6e68c78
marugy <gyulife7301@naver.com>
2023-08-08 00:58:00 +0900

    S09P12C203-411 / feat / openvidu API 구현

--- backend/src/main/java/reon/app/domain/openvidu/api/OpenViduApi.java
+++ backend/src/main/java/reon/app/domain/openvidu/api/OpenViduApi.java
@@ -1,117 +1,125 @@
 package reon.app.domain.openvidu.api;
 
-
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Value;
+import io.openvidu.java.client.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import io.openvidu.java.client.Connection;
-import io.openvidu.java.client.ConnectionProperties;
-import io.openvidu.java.client.OpenVidu;
-import io.openvidu.java.client.OpenViduHttpException;
-import io.openvidu.java.client.OpenViduJavaClientException;
-import io.openvidu.java.client.Session;
-import io.openvidu.java.client.SessionProperties;
+import reon.app.domain.openvidu.service.OpenViduService;
 
+
+@Tag(name = "Openvidu", description = "Openvidu API")
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/openvidu-management")
+@RequiredArgsConstructor
 public class OpenViduApi {
 
-    @Value("${OPENVIDU_URL}")
-    private String OPENVIDU_URL;
-
-    @Value("${OPENVIDU_SECRET}")
-    private String OPENVIDU_SECRET;
-
-    private OpenVidu openvidu;
-
-    @PostConstruct
-    public void init() {
-        this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
-    }
+    private final OpenViduService openViduService;
     /**
      * @param params The Session properties
      * @return The Session ID
      */
-    @PostMapping("/api/sessions")
+    @Operation(summary = "세션만 생성",description = "params로 세션 아이디가 주어지면 해당 아이디로 세션 생성, 없으면 세션 자동 생성")
+    @PostMapping("/sessions")
     public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)  throws OpenViduJavaClientException, OpenViduHttpException {
-        SessionProperties properties = SessionProperties.fromJson(params).build();
-        Session session = openvidu.createSession(properties);
-        return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
+//        SessionProperties properties = SessionProperties.fromJson(params).build();
+//        Session session = openvidu.createSession(properties);
+//        return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
+
+        String sessionId;
+        if(params==null){
+            sessionId = openViduService.startSession();
+        }else{
+            sessionId = openViduService.startSessionBySessionId(params);
+        }
+        return new ResponseEntity<>(sessionId, HttpStatus.OK);
     }
     /**
      * @param sessionId The Session in which to create the Connection
      * @param params    The Connection properties
      * @return The Token associated to the Connection
      */
-    @PostMapping("/api/sessions/{sessionId}/connections")
+    @Operation(summary = "connection 생성",description = "sessionId에 해당하는 connecntion 생성 후 token 반환")
+    @PostMapping("/sessions/{sessionId}/connections")
     public ResponseEntity<String> createConnection(@PathVariable("sessionId") String sessionId,
                                                    @RequestBody(required = false) Map<String, Object> params)
             throws OpenViduJavaClientException, OpenViduHttpException {
-        Session session = openvidu.getActiveSession(sessionId);
-        if (session == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
-        Connection connection = session.createConnection(properties);
-        return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
-    }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    @PostMapping("/test")
-    public ResponseEntity<String> test(@RequestBody(required = false) Map<String, Object> params)
-            throws OpenViduJavaClientException, OpenViduHttpException {
-
-//        SessionProperties properties = SessionProperties.fromJson(params).build();
-//        Session session = openvidu.createSession(properties);
-//
-//        String sessionId = session.getSessionId();
-//
-//        Session findSession = openvidu.getActiveSession(sessionId);
-//
-//        if (findSession == null) {
+//        Session session = openvidu.getActiveSession(sessionId);
+//        if (session == null) {
 //            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 //        }
-//        ConnectionProperties cproperties = ConnectionProperties.fromJson(params).build();
-//        Connection connection = findSession.createConnection(cproperties);
-
-        //세션 목록 가져오기
-        openvidu.fetch();
-        List<Session> sessions = openvidu.getActiveSessions();
-        //세션 목록이 있을때
-        if(sessions.size()>0) {
-            System.out.println(sessions.size());
-            Session targetSession = null;
-            //세션들 입장 인원이 1명인 세션 확인하기.
-            for (int i = 0; i < sessions.size(); i++) {
-                //검사할 세션
-                Session session = sessions.get(i);
-                //해당 세션의 커넥션
-                List<Connection> connections = session.getConnections();
-                //1명만 연결되어 있는 경우
-                if (connections.size() == 1) {
-                    //커넥션 생성 후 반환
-                    ConnectionProperties cproperties = ConnectionProperties.fromJson(params).build();
-                    Connection connection = session.createConnection(cproperties);
-                    return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
-                }
-            }
-        }
-        System.out.println("0개임");
-        //세션 목록이 없거나 1명인 세션이 없는 경우 새롭게 새션 생성
-        SessionProperties properties = SessionProperties.fromJson(params).build();
-        Session session = openvidu.createSession(properties);
-        ConnectionProperties cproperties = ConnectionProperties.fromJson(params).build();
-        Connection connection = session.createConnection(cproperties);
-        return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
-    }
+//        ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+//        Connection connection = session.createConnection(properties);
 //        return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+        ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+        String token = openViduService.createConnection(sessionId,properties );
+        if(token==null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+    @Operation(summary = "배틀 세션 받기",description = "배틀룸에서 사용할 세션 정보 및 토큰 반환")
+    @PostMapping("/sessions/connections")
+    public ResponseEntity<String> participateRankGame(@RequestBody(required = false) Map<String, Object> params)
+            throws OpenViduJavaClientException, OpenViduHttpException {
 
+//        //세션 목록 가져오기
+//        openvidu.fetch();
+//        List<Session> sessions = openvidu.getActiveSessions();
+//        //세션 목록이 있을때
+//        if(sessions.size()>0) {
+//            System.out.println(sessions.size());
+//            Session targetSession = null;
+//            //세션들 입장 인원이 1명인 세션 확인하기.
+//            for (int i = 0; i < sessions.size(); i++) {
+//                //검사할 세션
+//                Session session = sessions.get(i);
+//                //해당 세션의 커넥션
+//                List<Connection> connections = session.getConnections();
+//                //1명만 연결되어 있는 경우
+//                if (connections.size() == 1) {
+//                    //커넥션 생성 후 반환
+//                    ConnectionProperties cproperties = ConnectionProperties.fromJson(params).build();
+//                    Connection connection = session.createConnection(cproperties);
+//                    return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+//                }
+//            }
+//        }
+//        System.out.println("0개임");
+//        //세션 목록이 없거나 1명인 세션이 없는 경우 새롭게 새션 생성
+//        SessionProperties properties = SessionProperties.fromJson(params).build();
+//        Session session = openvidu.createSession(properties);
+//        ConnectionProperties cproperties = ConnectionProperties.fromJson(params).build();
+//        Connection connection = session.createConnection(cproperties);
+//        return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+//    }
+        //property 생성
+        SessionProperties properties = SessionProperties.fromJson(params).build();
+        ConnectionProperties cproperties = ConnectionProperties.fromJson(params).build();
+        //세션 목록 조회
+        List<Session> sessions = openViduService.getActiveSessions();
+        if(sessions!=null){
+            String token = openViduService.participateRankRoom(sessions,cproperties);
+            if(token!=null)
+                return new ResponseEntity<>(token, HttpStatus.OK);
+        }
+        String token = openViduService.makeRankRoom(properties,cproperties);
+        return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+
+    @Operation(summary = "배틀 세션 종료",description = "sessionId에 해당하는 세션 종료")
+    @PostMapping("/sessions/{sessionId}/delete")
+    public ResponseEntity<String> endSession(@PathVariable("sessionId") String sessionId)
+            throws OpenViduJavaClientException, OpenViduHttpException {
+            openViduService.deleteSession(sessionId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
 
--- backend/src/main/java/reon/app/domain/openvidu/service/OpenViduService.java
+++ backend/src/main/java/reon/app/domain/openvidu/service/OpenViduService.java
@@ -1,4 +1,16 @@
 package reon.app.domain.openvidu.service;
 
+import io.openvidu.java.client.*;
+
+import java.util.List;
+import java.util.Map;
+
 public interface OpenViduService {
+    String startSession() throws OpenViduJavaClientException, OpenViduHttpException;
+    String startSessionBySessionId(Map<String, Object> params)throws OpenViduJavaClientException, OpenViduHttpException;
+    String createConnection(String sessionId, ConnectionProperties properties)throws OpenViduJavaClientException, OpenViduHttpException;
+    List<Session> getActiveSessions() throws OpenViduJavaClientException, OpenViduHttpException;
+    String participateRankRoom(List<Session> sessions, ConnectionProperties cproperties)throws OpenViduJavaClientException, OpenViduHttpException;
+    String makeRankRoom(SessionProperties properties, ConnectionProperties cproperties)throws OpenViduJavaClientException, OpenViduHttpException;
+    void deleteSession(String sessionId) throws OpenViduJavaClientException, OpenViduHttpException;
 }
