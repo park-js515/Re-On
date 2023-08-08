5231b4435cb0ad766fad14094e1d2097e939f22e
marugy <gyulife7301@naver.com>
2023-08-08 20:49:58 +0900

    S09P12C203-336 / refactor / Auth Swagger 수정

--- backend/src/main/java/reon/app/domain/auth/api/AuthController.java
+++ backend/src/main/java/reon/app/domain/auth/api/AuthController.java
@@ -1,5 +1,8 @@
 package reon.app.domain.auth.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +14,15 @@ import reon.app.domain.auth.params.NaverLoginParams;
 import reon.app.domain.auth.service.OAuthLoginService;
 import reon.app.domain.auth.tokens.AuthTokens;
 
+@Tag(name="Auth",description = "SocialLogin")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
 @Slf4j
 public class AuthController {
     private final OAuthLoginService oAuthLoginService;
-
+    @Tag(name="Auth",description = "SocialLogin")
+    @Operation(summary = "네이버 로그인",description = "params로 인가코드를 주면 AccessToken 및 RefeshToken 반환")
     @PostMapping("/naver")
     public ResponseEntity<AuthTokens> loginNaver(@RequestBody NaverLoginParams params){
         return ResponseEntity.ok(oAuthLoginService.login(params));
