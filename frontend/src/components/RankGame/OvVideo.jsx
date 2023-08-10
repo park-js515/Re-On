aaa061cfb154186740f3f3517c8216357036cea0
PARKHEECHANG <shjc4623@gmail.com>
2023-08-10 17:57:26 +0900

    S09P12C203-187 / chore / 랭크게임 녹화본 따기

--- frontend/src/components/RankGame/OvVideo.jsx
+++ frontend/src/components/RankGame/OvVideo.jsx
@@ -1,7 +1,66 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function OpenViduVideoComponent({ streamManager, mySide}) {
+export default function OpenViduVideoComponent({
+  streamManager,
+  mySide,
+  recordOn,
+  userCamBorder,
+}) {
   const videoRef = useRef();
+  const [mediaRecorder, setMediaRecorder] = useState(null);
+  const [recording, setRecording] = useState(false);
+  const constraints = { audio: true, video: true };
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function (mediaStream) {
+        if (videoRef.current) {
+          videoRef.current.srcObject = mediaStream;
+        }
+
+        const recordedChunksLocal = [];
+
+        const recorder = new MediaRecorder(mediaStream);
+        recorder.ondataavailable = (e) => {
+          if (e.data.size > 0) {
+            recordedChunksLocal.push(e.data);
+          }
+        };
+        recorder.onstop = () => {
+          const blob = new Blob(recordedChunksLocal, { type: 'video/mp4' });
+          const url = URL.createObjectURL(blob);
+
+          // FormData 객체를 사용하여 파일을 서버로 전송
+          // const formData = new FormData();
+          // formData.append('file', blob);
+
+          // 서버에 파일 업로드
+          // axios
+          //   .post('/upload', formData, {
+          //     headers: {
+          //       'Content-Type': 'multipart/form-data',
+          //     },
+          //   })
+          //   .then((response) => {
+          //     console.log('File uploaded successfully:', response.data);
+          //   })
+          //   .catch((error) => {
+          //     console.error('File upload failed:', error);
+          //   });
+
+          const myFile = new File([url], 'demo.mp4', { type: 'video/mp4' });
+          console.log(myFile);
+
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = 'recorded-video.mp4'; // 저장될 파일 이름
+          // a.click();
+        };
+        setMediaRecorder(recorder);
+      });
+  }, []);
 
   useEffect(() => {
     if (streamManager && videoRef.current) {
@@ -9,7 +68,31 @@ export default function OpenViduVideoComponent({ streamManager, mySide}) {
     }
   }, [streamManager]);
 
-  console.log('mySide@@@@@@@@@@@@@@@@@@', mySide)
+  useEffect(() => {
+    if (mediaRecorder) {
+      if (recordOn && !recording) {
+        mediaRecorder.start();
+        setRecording(true);
+        console.log('녹화 시작');
+      } else if (!recordOn && recording) {
+        mediaRecorder.stop();
+        setRecording(false);
+        console.log('녹화 중지');
+      }
+    }
+  }, [recordOn, mediaRecorder, recording]);
 
-  return <video id={mySide} autoPlay={true} ref={videoRef} className="rounded-lg" width={500} height={600}/>;
+  // 스트림매니저가 퍼블리셔이면서 record on이 넘어올때만 녹화하도록
+
+  // 스트림매니저가 퍼블리셔이면서 record off가 넘어올 때 녹화 종료하도록
+
+  return (
+    <video
+      id={mySide}
+      autoPlay={true}
+      ref={videoRef}
+      className={`rounded-lg ${userCamBorder ? 'border-4 border-danger' : ''}`}
+      style={{ width: '500px', height: '400px' }}
+    />
+  );
 }
--- frontend/src/components/RankGame/UserVideoComponent.jsx
+++ frontend/src/components/RankGame/UserVideoComponent.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import OpenViduVideoComponent from './OvVideo';
 
-export default function UserVideoComponent({ streamManager, mySide }) {
+export default function UserVideoComponent({
+  streamManager,
+  mySide,
+  recordOn,
+  userCamBorder,
+}) {
   const getNicknameTag = () => {
     // Gets the nickName of the user
     return JSON.parse(streamManager.stream.connection.data).clientData;
@@ -16,7 +21,12 @@ export default function UserVideoComponent({ streamManager, mySide }) {
               {getNicknameTag()}
             </p>
           </div>
-          <OpenViduVideoComponent streamManager={streamManager} mySide={mySide}/>
+          <OpenViduVideoComponent
+            streamManager={streamManager}
+            mySide={mySide}
+            recordOn={recordOn}
+            userCamBorder={userCamBorder}
+          />
         </div>
       ) : null}
     </div>
