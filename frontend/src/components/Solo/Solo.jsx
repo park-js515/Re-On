82483fba159dc8da3fe86c6827b9c2bc70184c08
안종상 <skqlck-@naver.com>
2023-08-11 15:07:22 +0900

    S09P12C203-446 / feat / 혼자하기 페이지 기능 구현

--- frontend/src/components/Solo/Solo.jsx
+++ frontend/src/components/Solo/Solo.jsx
@@ -1,25 +1,45 @@
-import {React, useState, useEffect, useRef } from "react";
+import { React, useState, useEffect, useRef } from "react";
 import * as faceapi from 'face-api.js'
 import './Solo.css'
-const ort = require('onnxruntime-web/webgpu')
 
-const SoloApp = ({video_url}) => {
-  const [ortSession, setOrtSession] = useState(null);
-  const video_width = 500;
-  const video_height = 400;
+
+const SoloApp = () => {
+
+  const ort = require('onnxruntime-web/webgpu')
+
+  // ※ 이거 바꾸시려면 Solo.css도 바꾸셔야 합니다. //
+  const video_width = 500; //
+  const video_height = 400;//
+  ///////////////////////////
+  
   const [reload, setReload] = useState(false);
+  const videoRef = useRef();
+  const webCamRef = useRef();
+  const [answer, setAnswer] = useState(0);
+
+  // API 연결되면 수정해야 될 부분
+  const urls = [
+    'video/ISawTheDevil.mp4',
+    'video/아저씨-원빈-금니빨.mp4',
+  ]
+  const [url, setUrl] = useState(urls[Math.round(Math.random())])
+
+  const getURL = () => {
+    setUrl(urls[Math.round(Math.random())])
+  }
+
+  const [ortSession, setOrtSession] = useState(null);
+
   useEffect(()=>{
     async function createSession(){
       try {
         await setOrtSession(await ort.InferenceSession.create('reon_model-2.onnx', {executionProviders: ['webgl']}));
         await faceapi.nets.tinyFaceDetector.loadFromUri('models');
-        
-        // OpenVidu 안쓰고 합니다.
-        const video = document.getElementById('webCam')
+        console.log('ONNX, Face-API 로딩 완료')
         if (navigator.mediaDevices.getUserMedia){
           navigator.mediaDevices.getUserMedia({video:true})
           .then((stream)=>{
-            video.srcObject = stream
+            webCamRef.current.srcObject = stream;
           })
           .catch((error)=>{
             console.log(error)
@@ -31,17 +51,53 @@ const SoloApp = ({video_url}) => {
         document.write(`failed to inference ONNX model: ${e}.`)
       }
     };
+  
     createSession();
-    console.log("etest")
-    setTimeout(()=>{})
-    return ()=>{setOrtSession(null)}
-  },[])
 
-  /////////////////////////// 만지면 안되는 부분 //////////////////////////////////
-  let detectInterval = null;
+    return ()=>{setOrtSession(null);faceapi.tf.dispose();console.log("전부 삭제")}
+  },[])
+  
+  useEffect(() => {
+    if (videoRef.current) {
+      const handleEnded = () => {
+        clearInterval(detectInterval)
+        while (document.getElementsByTagName('canvas').length > 0){
+          document.getElementsByTagName('canvas')[0].remove()
+        }
+        videoRef.current.currentTime = 0;
+        setReload((reload)=>{return !reload})
+        setAnswer((frame_cnts === 0) ? 0 : 100 - (sum_diff / frame_cnts)*100)
+        sum_diff = 0;
+        frame_cnts = 0;
+      };
+      const handlePlay = () => {
+        face_detect();
+      }
+
+      videoRef.current.addEventListener('ended', handleEnded);
+      videoRef.current.addEventListener('play', handlePlay);
+
+      return () => {
+        clearInterval(detectInterval)
+        while (document.getElementsByTagName('canvas').length > 0){
+          document.getElementsByTagName('canvas')[0].remove()
+        }
+        if (videoRef.current) {
+          videoRef.current.removeEventListener('ended', handleEnded);
+          videoRef.current.removeEventListener('play', handlePlay);
+        }
+      };
+    }
+  },[videoRef]);
+
+  ///////////////////////////////////////// 초롱이 시작 ////////////////////////////////////////////////
+  let detectInterval = null;                                                                        ///
+  let frame_cnts = 0;                                                                               ///
+  let sum_diff = 0;
+
   function face_detect() {
-    const video1 = document.getElementById("webCam");
-    const video2 = document.getElementById("movie");
+    const video1 = webCamRef.current
+    const video2 = videoRef.current
     const canvas1 = faceapi.createCanvasFromMedia(video1);
     const canvas2 = faceapi.createCanvasFromMedia(video2);
     const displaySize = { width: video_width, height: video_height };
@@ -71,14 +127,12 @@ const SoloApp = ({video_url}) => {
   }
   
   async function image_classification(box1, box2) {
-
     const [x1, y1, w1, h1] = [box1.x, box1.y, box1.width, box1.height];
     const [x2, y2, w2, h2] = [box2.x, box2.y, box2.width, box2.height];
     const mean = [0.485, 0.456, 0.406];
     const std = [0.229, 0.224, 0.225];
-    const imageData1 = resizeImage(x1,y1,w1,h1,"webCam")
-    const imageData2 = resizeImage(x2,y2,w2,h2,"movie")
-
+    const imageData1 = resizeImage(x1,y1,w1,h1,webCamRef.current)
+    const imageData2 = resizeImage(x2,y2,w2,h2,videoRef.current)
 
     // Create a new Float32Array for the input tensor
     const inputData1 = new Float32Array(1 * 3 * 224 * 224);
@@ -105,35 +159,42 @@ const SoloApp = ({video_url}) => {
     const input2 = { [inputName]: inputTensor2 };
     const output1 = await ortSession.run(input1);
     const output2 = await ortSession.run(input2);
-    let probs1 = output1[outputName].data
-    let probs2 = output2[outputName].data
-    // console.log(probs1)
-    // console.log(probs2)
-    // console.log("==========================")
+    let probs1 = output1[outputName].data;
+    let probs2 = output2[outputName].data;
+    const total1 = probs1.reduce((a, b) => a + Math.exp(b), 0);
+    const total2 = probs2.reduce((a, b) => a + Math.exp(b), 0);
+    probs1 = probs1.map((prob) => {
+      return (Math.exp(prob) / total1).toFixed(2);
+    });
+    probs2 = probs2.map((prob) => {
+      return (Math.exp(prob) / total2).toFixed(2);
+    });
+    for (let i = 0; i < 7; i++) {
+      sum_diff += Math.abs(probs1[i] - probs2[i]);
+    }
+    frame_cnts++;
   }
 
-  function resizeImage(x,y,w,h,id) {
-    const video = document.getElementById(id)
+  function resizeImage(x,y,w,h,video) {
     const cropCanvas = document.createElement('canvas');
     cropCanvas.width = 224;
     cropCanvas.height = 224;
     const cropContext = cropCanvas.getContext('2d');
 
     cropContext.drawImage(video, x, y, w, h, 0, 0, 224, 224);
-    return cropContext.getImageData(0, 0, 224, 224);
-  }
-  /////////////////////////// 만지면 안되는 부분 //////////////////////////////////
+    return cropContext.getImageData(0, 0, 224, 224);                                     ///
+  }                                                                                      ///
+  //////////////////////////////////// 초롱이 끝 ////////////////////////////////////////////
 
   function startActing () {
-    if (reload){
-      clearInterval(detectInterval)
-      document.getElementsByTagName('canvas')[0].remove()
-      document.getElementsByTagName('canvas')[0].remove()
-      return;
+    if (reload) {
+      videoRef.current.currentTime = videoRef.current.duration
     }
-    const video = document.getElementById('movie')
-    video.play()
-    setReload(!reload)
+    else {
+      const video = videoRef.current
+      video.play()
+      setReload(!reload)
+    }
   }
 
   return (
@@ -146,34 +207,45 @@ const SoloApp = ({video_url}) => {
                 autoPlay
                 style={{ width: '500px', height: '400px' }}
                 className="rounded-lg"
+                ref={webCamRef}
               >
               </video>
           </div>
 
-          <button 
-            className="outline text-sm"
-            onClick={startActing}
-          >
-            {reload ? "RESTART" : "START"}
-          </button>
+          <div className="flex flex-col">
+            {/* 재시작 버튼 */}
+            <button 
+              className="outline text-md rounded p-2 text-center"
+              onClick={startActing}
+              disabled={ortSession ? false : true}
+            >
+              {reload ? "RESTART" : "START"}
+            </button>
+            {/* 영화 바꾸기 버튼 */}
+            <button 
+              className="mt-2 outline text-md rounded p-2 text-center"
+              onClick={getURL}
+              disabled={reload ? true : false}
+            >
+              Change Movie
+            </button>
+            {/* 점수 */}
+            <span className="mt-2 rounded outline outline-green text-md p-2 text-center">점수 : {answer.toFixed(2)}</span>
+          </div>
 
           <div id="movie_container">
 
               <video id="movie"
-                src={video_url}
+                src={url}
                 style={{ width: '500px', height: '400px' }}
-                onPlay={face_detect}
-                onEnded={()=>{
-                  clearInterval(detectInterval)
-                  document.getElementsByTagName('canvas')[0].remove()
-                  document.getElementsByTagName('canvas')[0].remove()
-                }}
                 className="rounded-lg"
+                ref={videoRef}
               >
               </video>
           </div>
 
       </div>
+
     </div>
   )
 }
--- frontend/src/pages/NormalListPage.jsx
+++ frontend/src/pages/NormalListPage.jsx
@@ -1,23 +1,13 @@
 import SoloApp from "components/Solo/Solo";
-import { useState } from "react";
 
 const NoramalListPage = () => {
-  const urls = [
-    'video/ISawTheDevil.mp4',
-    'video/아저씨-원빈-금니빨.mp4',
-  ]
-  const [cnt, setCnt] = useState(0)
-  const [url,setUrl] = useState(urls[0])
 
-  const getURL = () => {
-    setUrl(urls[(cnt+1)%urls.length])
-    setCnt(cnt+1)
-  }
   return (
+
     <div>
-      <SoloApp video_url={url}></SoloApp>
-      <button onClick={getURL}>change</button>
+      <SoloApp></SoloApp>
     </div>
+
   );
 };
 
