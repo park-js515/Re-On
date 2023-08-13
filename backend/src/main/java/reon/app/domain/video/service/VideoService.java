f56556c8ee5c1d183ad103f8c4ff4c87d9d878fe
eat159357 <eat159357@kookmin.ac.kr>
2023-08-14 05:17:57 +0900

    S09P12C203-470 / feat / video save 추가

---
+++ backend/src/main/java/reon/app/domain/video/service/VideoService.java
@@ -0,0 +1,36 @@
+package reon.app.domain.video.service;
+
+import com.google.cloud.storage.Storage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reon.app.domain.video.entity.Video;
+import reon.app.domain.video.repository.VideoRepository;
+import reon.app.domain.video.service.dto.VideoSaveDto;
+import reon.app.global.util.FileManger;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class VideoService {
+    private final VideoRepository videoRepository;
+    private final Storage storage;
+    private FileManger fileManger = new FileManger();
+
+    public void save(VideoSaveDto videoSaveDto) {
+        String videoPath = fileManger.updateImgFile(videoSaveDto.getOriginalVideo(), storage);
+        String thumbnail = fileManger.updateImgFile(videoSaveDto.getThumbnail(), storage);
+
+        Video video = Video.builder()
+                .title(videoSaveDto.getTitle())
+                .actor(videoSaveDto.getActor())
+                .script(videoSaveDto.getScript())
+                .sttScript(videoSaveDto.getSttScript())
+                .videoPath(videoPath)
+                .thumbnail(thumbnail)
+                .build();
+        videoRepository.save(video);
+
+
+    }
+}
